0b50ea52230554ba87dfc0d0cb64414a9a4531fa
Kamy Moussavi <32441857+ogKamy@users.noreply.github.com>
2019-07-11 14:58:50 -0400

    publish /status/motor topic

--- src/motor_control.py
+++ src/motor_control.py
@@ -31,22 +31,25 @@ class MotorControl(object):
         self.sub_right = rospy.Subscriber('/control/motor/cmd_vel', Twist, self.callback)
         
         # Publishing
-        self.pub_pwm = rospy.Publisher('/status/motor/pwm', Twist, queue_size=1)
+        self.pub_status = rospy.Publisher('/status/motor', MotorStatus, queue_size=1)
 
+        self.motor_status = MotorStatus()
         self.cmd_data = Twist()
         
         
         self.v_right = 0
         self.v_left = 0
-        self.pwm_right = 0
-        self.pwm_left = 0
-        self.dir_right = "FORWARDS"
-        self.dir_left = "FORWARDS"
+        self.right_pwm = 0
+        self.left_pwm = 0
+        self.left_reverse = False
+        self.right_reverse = False
         
     # Capture cmd_vel 
     def callback(self, data):
         self.cmd_data = data
         self.cmd_vel_to_diff(self.cmd_data)
+        self.turnWheels()
+        self.publish_status()
         #rospy.loginfo(self.cmd_data)
     
     #transform to differential drive velocities (right and left wheel velocities)
@@ -59,34 +62,39 @@ class MotorControl(object):
         #rospy.loginfo("DIFF SPEED (Left, Right): " + str(self.v_left) + ", " + str(self.v_right))
         
         if self.v_left >= 0:
-            self.dir_left = "FORWARDS"
+            self.right_reverse = False
             #self.left_pwm.enable()
-            self.pwm_left = abs(self.v_left / VEL_TO_PWM_FACTOR)
+            self.left_pwm = abs(self.v_left / VEL_TO_PWM_FACTOR)
         else:
-            self.dir_left= "BACKWARDS" 
+            self.right_reverse= True
             #self.left_pwm.disable()
             #Flipping left and right when reverse
-            self.pwm_left = abs(self.v_right / VEL_TO_PWM_FACTOR) 
+            self.left_pwm = abs(self.v_right / VEL_TO_PWM_FACTOR) 
  
         if self.v_right >= 0:
-            self.dir_right = "FORWARDS"
+            self.left_reverse = False
             #self.left_pwm.enable()
-            self.pwm_right = abs(self.v_right / VEL_TO_PWM_FACTOR)
+            self.right_pwm = abs(self.v_right / VEL_TO_PWM_FACTOR)
         else:
-            self.dir_right = "BACKWARDS" 
+            self.left_reverse = True
             #self.left_pwm.disable()
             #Flipping left and right when reverse 
-            self.pwm_right = abs(self.v_left / VEL_TO_PWM_FACTOR)
+            self.right_pwm = abs(self.v_left / VEL_TO_PWM_FACTOR)
 
-        self.turnWheels()
-
-    def turnWheels(self):
-        rospy.loginfo("DIR (Left, Right): " + str(self.dir_left) + ", " + str(self.dir_right))
-        rospy.loginfo("PWM (Left, Right): " + str(self.pwm_left) + ", " + str(self.pwm_right))
+    def turn_wheels(self):
+        #rospy.loginfo("DIR (Left, Right): " + str(self.right_reverse) + ", " + str(self.left_reverse))
+        #rospy.loginfo("PWM (Left, Right): " + str(self.left_pwm) + ", " + str(self.right_pwm))
         
-        self.right_pwm.set_duty_cycle(self.pwm_right)
-        self.left_pwm.set_duty_cycle(self.pwm_left)
+        self.right_pwm.set_duty_cycle(self.right_pwm)
+        self.left_pwm.set_duty_cycle(self.left_pwm)
 
+    def publish_status(self):
+        self.motor_status.left_pwm = self.left_pwm
+        self.motor_status.right_pwm = self.left_pwm
+        self.motor_status.left_reverse = self.left_reverse
+        self.motor_status.right_reverse = self.right_reverse
+        
+        self.pub_status.publish(self.motor_status)
 
     def initialize_pwm(self):
         self.right_pwm.initialize()
