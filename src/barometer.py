774dd1bbce3527b31531faf42f7dbba41003912e
Kamy Moussavi <32441857+ogKamy@users.noreply.github.com>
2019-07-15 14:21:05 -0400

    barometer

---
+++ src/barometer.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+import navio.adc
+import navio.ms5611
+import navio.util
+
+import rospy
+from yqb_car.msg import Barometer
+
+
+class BarometerReader(object):
+    def __init__(self):
+        self.pub = rospy.Publisher('/sensor/barometer', Barometer, queue_size=1)
+        self.baro = navio.ms5611.MS5611()
+        self.baro.initialize()   
+        self.baro_data = Barometer()
+
+    def get_data(self):
+        self.baro.refreshPressure()
+        self.baro.readPressure()
+
+        self.baro.refreshTemperature()
+        self.baro.readTemperature()
+
+        self.baro.calculatePressureAndTemperature()
+
+        print "Temperature(C): %.6f" % (self.baro.TEMP), "Pressure(millibar): %.6f" % (self.baro.PRES)
+        
+        self.baro_data.temperature = self.baro.TEMP
+        self.baro_data.pressure = self.baro.PRES
+        self.pub.publish(self.baro_data)
+
+if __name__ == "__main__":
+    rospy.init_node('barometer')
+    barometer_reader = BarometerReader()
+
+    rate = rospy.Rate(1)
+
+    while not rospy.is_shutdown():
+        barometer_reader.get_data()
+        rate.sleep()
