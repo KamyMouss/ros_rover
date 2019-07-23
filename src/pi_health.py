602a575a73b8781c3c4c5eb7b94830da98de3852
Kamy Moussavi <32441857+ogKamy@users.noreply.github.com>
2019-07-23 09:44:34 -0400

    Update pi_health.py

--- src/pi_health.py
+++ src/pi_health.py
@@ -12,7 +12,7 @@ class PiHealthReader(object):
         print psutil.cpu_percent(interval=1)
         print psutil.virtual_memory()
         print psutil.disk_usage('/')
-        print psutil.sensors_temperatures(
+        print psutil.sensors_temperatures()
 
 if __name__ == "__main__":
     rospy.init_node('pi_health')
