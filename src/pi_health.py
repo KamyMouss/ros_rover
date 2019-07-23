cbeaefb99bb425768872a4802742897b7f0dfd2b
ogKamy <kamy.moussavi@gmail.com>
2019-07-23 16:54:09 +0000

    percentages print

--- src/pi_health.py
+++ src/pi_health.py
@@ -2,17 +2,23 @@
 
 import rospy
 import psutil
+from subprocess import PIPE, Popen
 from std_msgs.msg import Empty
 
 class PiHealthReader(object):
     def __init__(self):
         self.pub = rospy.Publisher('/health/pi', Empty, queue_size=1)
 
+    def get_cpu_temperature(self):
+        process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
+        output, _error = process.communicate()
+        return float(output[output.index('=') + 1:output.rindex("'")])
+
     def get_health(self):
         print psutil.cpu_percent(interval=1)
-        print psutil.virtual_memory()
-        print psutil.disk_usage('/')
-        print psutil.sensors_temperatures(
+        print psutil.virtual_memory()[2]
+        print psutil.disk_usage('/')[3]
+	print self.get_cpu_temperature()
 
 if __name__ == "__main__":
     rospy.init_node('pi_health')
